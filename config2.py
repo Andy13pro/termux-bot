36edcba3dd58273c9888b3ce24b0a86ed3a920c9
Andy13pro <122709748+Andy13pro@users.noreply.github.com>
2024-12-19 20:58:34 -0500

    Add files via upload

---
+++ config2.py
@@ -0,0 +1,13 @@
+# config2.py
+api_id = '24128308'
+api_hash = 'e1d006e1aede7e1159b55148232780d7'
+makidox_bot_username = 'PisllingDOX_bot'
+session_name = 'leoVidal_session'
+
+# Diccionario con los nombres de usuario de los bots
+bots_usernames = {
+    'fenix': '@FENIX_FX_BOT',  # Nombre de usuario del bot Fenix
+    'rave': 'SDBB_Bot', # Nombre de usuario de otro bot adicional
+     'Lain': 'LainDoxBeta_Bot'  # Nombre de usuario de otro bot adicional
+
+}
